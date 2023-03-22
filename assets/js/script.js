9ccac46fdc014fa1699a9a0601f1947ac8cf4958
Miguel Rodríguez <miguelillo.rs02@gmail.com>
2023-03-22 23:44:10 +0100

    FAQ

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,3 +112,23 @@ $(document).ready(function() {
     });
 
 });
+
+//FAQ
+const faq = document.getElementsByClassName("faq-question");
+let i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("faq-active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
