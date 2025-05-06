a646dcf73c7d66d7ee2fbd723a7abe20af82a168
Sergio Conejero <sergio.conejero@multitecua.com>
2025-05-06 19:01:19 +0200

    Add lixsa script

---
+++ assets/js/lixsa.js
@@ -0,0 +1,8 @@
+!(function(lix,s,a){
+  ((s=lix.createElement("script")).src="https://chat.lixsa.ai/lixsa-chat.umd.cjs"),
+  s.setAttribute("data-language", "es"),
+  s.setAttribute("data-welcome-message", "¡Hola! ¿Cómo puedo ayudarte?"),
+  s.setAttribute("data-lixsa-id", "55e502ac-38be-4b52-a203-0e4bde48746f"),
+  (s.async=true),
+  (a=lix.getElementsByTagName("script")[0]).parentNode.insertBefore(s,a);
+})(document); 
