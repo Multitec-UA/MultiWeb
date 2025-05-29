5fdfe10c12a60190394809a4f048ab0f51d20379
Sergio Conejero <sergio.conejero@multitecua.com>
2025-05-29 18:21:35 +0200

    Update lixsa theme script

--- assets/js/lixsa.js
+++ assets/js/lixsa.js
@@ -3,6 +3,7 @@
   s.setAttribute("data-language", "es"),
   s.setAttribute("data-welcome-message", "¡Hola! ¿Cómo puedo ayudarte?"),
   s.setAttribute("data-lixsa-id", "55e502ac-38be-4b52-a203-0e4bde48746f"),
+  s.setAttribute("data-theme", '{"--lixsa-primary":"#ba1616","--lixsa-primary-gradient":"linear-gradient(135deg, #ba1616, #e23b3b)","--lixsa-text":"#333333","--lixsa-text-user":"#ffffff","--lixsa-text-light":"#555555","--lixsa-text-light-secondary":"#dddddd","--lixsa-bg":"#f5f5f5","--lixsa-bg-secondary":"#ffffff","--lixsa-shadow-sm":"0 1px 3px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.03)","--lixsa-shadow-md":"0 4px 6px -1px rgba(0, 0, 0, 0.08), 0 2px 4px -1px rgba(0, 0, 0, 0.04)","--lixsa-shadow-lg":"0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.05)","--lixsa-radius-sm":"0.375rem","--lixsa-radius-md":"0.5rem","--lixsa-radius-lg":"1rem","--lixsa-radius-full":"9999px","--lixsa-transition":"all 0.2s cubic-bezier(0.4, 0, 0.2, 1)","--lixsa-font-family":"-apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif"}'),
   (s.async=true),
   (a=lix.getElementsByTagName("script")[0]).parentNode.insertBefore(s,a);
 })(document); 
